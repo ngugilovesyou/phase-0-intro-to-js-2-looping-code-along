9b8d65f1a9116c4da2c22b582b11db111e6fb7f1
Samuel <amsamgitau@gmail.com>
2024-06-30 15:55:33 +0300

    complete test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+function writeCards( guests, event ) {
+    let thankYouCards = []
+    for ( let i = 0; i < guests.length; i++ ) {
+      thankYouCards.push( `Thank you, ${guests[i]}, for the wonderful ${event} gift!` )
+    }
+    return thankYouCards
+  }
+writeCards(["Charlie", "Samip", "Ali"], "birthday");
+
+function countDown( startingNumber ) {
+    while ( startingNumber > 0 ) {
+      console.log( startingNumber );
+      startingNumber -= 1;
+    }
+    console.log( startingNumber );
+  }
